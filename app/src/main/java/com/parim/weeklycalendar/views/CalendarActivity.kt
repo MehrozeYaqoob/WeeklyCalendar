ccda9f2c3f609183c517909f54c823b7f52ea4b9
Mehroze <mehroze.yaqoob@venturedive.com>
2021-12-15 23:11:36 +0500

    UI for Drop down done

--- app/src/main/java/com/parim/weeklycalendar/views/CalendarActivity.kt
+++ app/src/main/java/com/parim/weeklycalendar/views/CalendarActivity.kt
@@ -1,7 +1,11 @@
 package com.parim.weeklycalendar.views
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DefaultItemAnimator
@@ -20,9 +24,18 @@ import com.parim.weeklycalendar.provider.HolidayServiceProvider
 import com.parim.weeklycalendar.utils.LinearLayoutManagerWithSmoothScroller
 import com.parim.weeklycalendar.viewmodels.CalendarViewModel
 import com.parim.weeklycalendar.viewmodels.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.coroutines.flow.collect
 import java.util.*
 import kotlin.collections.ArrayList
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
+
+
+
+
 
 class CalendarActivity : AppCompatActivity() {
 
@@ -43,6 +56,7 @@ class CalendarActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         onInitCalendar()
+        onConfigureSpinner()
         onProvideViewModel()
         onConfigureCalendarRecyclerView()
         onLoadCalendarRecyclerAdapter()
@@ -53,6 +67,14 @@ class CalendarActivity : AppCompatActivity() {
         onShowErrorMessage()
     }
 
+    private fun onConfigureSpinner() {
+        val aa: ArrayAdapter<*> = ArrayAdapter<Any?>(this, R.layout.simple_spinner_item, applicationContext.resources.getStringArray(com.parim.weeklycalendar.R.array.english_days))
+        aa.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        binding.root.daySpinner.adapter = aa
+        binding.root.daySpinner.setSelection(0,false)
+        binding.root.daySpinner.onItemSelectedListener = itemSelected
+    }
+
     private fun onInitCalendar() {
         date = Date()
         date.time = System.currentTimeMillis()
@@ -61,7 +83,7 @@ class CalendarActivity : AppCompatActivity() {
 
         endCal = Calendar.getInstance()
         endCal.time = date
-        endCal.add(Calendar.MONTH, 3)
+        endCal.add(Calendar.MONTH, 1)
     }
 
     private fun onProvideViewModel() {
@@ -131,6 +153,16 @@ class CalendarActivity : AppCompatActivity() {
             calendarViewModel.errorEventFlow.collect { errorEvent ->
                 Snackbar.make(binding.root, errorEvent.message, Snackbar.LENGTH_LONG).show()
             }
+        }
+    }
+
+    private val  itemSelected = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            Toast.makeText(applicationContext,"Clicked", Toast.LENGTH_LONG).show()
+        }
+
+        override fun onNothingSelected(p0: AdapterView<*>?) {
+            TODO("Not yet implemented")
         }
     }
 }
