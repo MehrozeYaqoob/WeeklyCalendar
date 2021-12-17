2b6252459f283bb4fe0320203323324ddc687e0d
Mehroze <mehroze.yaqoob@venturedive.com>
2021-12-17 14:35:13 +0500

    Firebase added with Splash

--- app/src/main/java/com/parim/weeklycalendar/views/SplashActivity.kt
+++ app/src/main/java/com/parim/weeklycalendar/views/SplashActivity.kt
@@ -10,6 +10,7 @@ import com.parim.weeklycalendar.BuildConfig
 import com.parim.weeklycalendar.databinding.ActivitySplashBinding
 import com.parim.weeklycalendar.manager.PreferenceManager
 import com.parim.weeklycalendar.utils.Constants
+import com.parim.weeklycalendar.utils.Constants.FIREBASE_KEY_REFRESH_LAPSE
 import java.util.*
 
 @SuppressLint("CustomSplashScreen")
@@ -25,7 +26,7 @@ class SplashActivity : AppCompatActivity() {
 
     private fun onInitFirebase(){
         val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
-        firebaseRemoteConfig.setDefaultsAsync(mapOf("refresh_lapse" to 10))
+        firebaseRemoteConfig.setDefaultsAsync(mapOf(FIREBASE_KEY_REFRESH_LAPSE to 10))
         val configBuilder = FirebaseRemoteConfigSettings.Builder()
         if (BuildConfig.DEBUG) {
             val cacheInterval: Long = 0
@@ -36,7 +37,7 @@ class SplashActivity : AppCompatActivity() {
         firebaseRemoteConfig.fetchAndActivate()
             .addOnCompleteListener(this@SplashActivity) { task ->
                 if (task.isSuccessful) {
-                    PreferenceManager.setDaysAfterRemoteDataFetched(this,firebaseRemoteConfig.getLong(Constants.FIREBASE_KEY_REFRESH_LAPSE))
+                    PreferenceManager.setDaysAfterRemoteDataFetched(this,firebaseRemoteConfig.getLong(FIREBASE_KEY_REFRESH_LAPSE))
                     val intent  =  Intent(this, CalendarActivity::class.java)
                     startActivity(intent)
                 }
