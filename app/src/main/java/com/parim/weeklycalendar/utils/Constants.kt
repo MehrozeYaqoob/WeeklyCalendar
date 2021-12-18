a564039baee8a0158274e8a78749c17b4a06b508
mehrozeyaqoob <mehrozeyaqub@gmail.com>
2021-12-18 18:06:16 +0500

    Sonar supported files added and bitbucket-pipelines.yml added

--- app/src/main/java/com/parim/weeklycalendar/utils/Constants.kt
+++ app/src/main/java/com/parim/weeklycalendar/utils/Constants.kt
@@ -6,5 +6,6 @@ object Constants {
     const val JSON_ATTRIBUTE_REASON = "reason"
 
     const val FIREBASE_KEY_REFRESH_LAPSE = "refresh_lapse"
+    const val SPLASH_TIME = 1000
 
 }
--- app/src/main/java/com/parim/weeklycalendar/views/SplashActivity.kt
+++ app/src/main/java/com/parim/weeklycalendar/views/SplashActivity.kt
@@ -3,6 +3,9 @@ package com.parim.weeklycalendar.views
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
@@ -11,6 +14,7 @@ import com.parim.weeklycalendar.databinding.ActivitySplashBinding
 import com.parim.weeklycalendar.manager.PreferenceManager
 import com.parim.weeklycalendar.utils.Constants
 import com.parim.weeklycalendar.utils.Constants.FIREBASE_KEY_REFRESH_LAPSE
+import com.parim.weeklycalendar.utils.Constants.SPLASH_TIME
 import java.util.*
 
 @SuppressLint("CustomSplashScreen")
@@ -38,9 +42,17 @@ class SplashActivity : AppCompatActivity() {
             .addOnCompleteListener(this@SplashActivity) { task ->
                 if (task.isSuccessful) {
                     PreferenceManager.setDaysAfterRemoteDataFetched(this,firebaseRemoteConfig.getLong(FIREBASE_KEY_REFRESH_LAPSE))
-                    val intent  =  Intent(this, CalendarActivity::class.java)
-                    startActivity(intent)
+                    onStartSplashHandler()
                 }
+            }.addOnFailureListener {
+                onStartSplashHandler()
             }
+    }
+
+    private fun onStartSplashHandler(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent  =  Intent(this, CalendarActivity::class.java)
+            startActivity(intent)
+        }, SPLASH_TIME.toLong())
     }
 }
